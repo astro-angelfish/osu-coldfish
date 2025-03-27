f1b9b47d38416dfa98559d61400a0f8a491b6167
yuanxy-acy <yuanxy_acy@163.com>
2025-03-27 22:30:11 +0800

    add team info

--- src/main/java/moe/orangemc/osu/coldfish/tournament/Team.java
+++ src/main/java/moe/orangemc/osu/coldfish/tournament/Team.java
@@ -1,4 +1,28 @@
 package moe.orangemc.osu.coldfish.tournament;
 
+import moe.orangemc.osu.al1s.api.mutltiplayer.MultiplayerTeam;
+import moe.orangemc.osu.al1s.api.user.User;
+
+import java.util.List;
+
 public class Team {
+    private final MultiplayerTeam teamColor;
+    private final String teamName;
+    private final List<User> users;
+    public Team(MultiplayerTeam teamColor, String teamName, List<User> users) {
+        this.teamColor = teamColor;
+        this.teamName = teamName;
+        this.users = users;
+    }
+    public MultiplayerTeam getTeamColor() {
+        return teamColor;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
 }
