bdc7c788ddc1dc6a089ff7158303110a5e269a83
Astro Angelfish <astroangelfish@orangemc.moe>
2025-04-08 14:16:24 +0800

    basic pool marks.

--- src/main/java/moe/orangemc/osu/coldfish/tournament/map/MapPool.java
+++ src/main/java/moe/orangemc/osu/coldfish/tournament/map/MapPool.java
@@ -2,18 +2,14 @@ package moe.orangemc.osu.coldfish.tournament.map;
 
 import moe.orangemc.osu.al1s.api.beatmap.Beatmap;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MapPool {
-    private static final Pattern mapIdPattern = Pattern.compile("^([a-zA-Z]+)(\\d+)$");
     private final Map<String, MapIdentifier> mapIdentifiers = new HashMap<>();
 
     public Beatmap getBeatmap(String id) {
-        Matcher matcher = mapIdPattern.matcher(id);
+        Matcher matcher = MapIdentifier.mapIdPattern.matcher(id);
 
         if (matcher.find()) {
             String mapId = matcher.group(1);
@@ -26,5 +22,25 @@ public class MapPool {
             }
         }
         throw new IllegalArgumentException("Map not found: " + id);
+    }
+
+    public MapIdentifier getMapIdentifier(String id) {
+        Matcher matcher = MapIdentifier.mapIdPattern.matcher(id);
+
+        if (matcher.find()) {
+            String mapId = matcher.group(1);
+            MapIdentifier mapIdentifier = mapIdentifiers.get(mapId);
+            if (mapIdentifier != null) {
+                return mapIdentifier;
+            } else {
+                throw new NoSuchElementException(mapId);
+            }
+        }
+
+        throw new IllegalArgumentException("Map not found: " + id);
+    }
+
+    public Set<MapIdentifier> getAvailableIdentifiers() {
+        return new HashSet<>(mapIdentifiers.values());
     }
 }
